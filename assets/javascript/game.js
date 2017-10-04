26525e16001b334ac413484c3b3e24946a0a9389
Deva <deva.palasingam@gmail.com>
2017-10-04 14:58:34 -0500

    working on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var words = ["typewriter", "pager", "fax machine", "dial up", "phonebook", "floppy disk", "VHS"];
+
+var guessedLetters = [];
+var trysLeft = 12;
+
+var chosenWord = words[Math.floor(Math.random() * words.length)];
+
+var wordLength = chosenWord.length;
+
+
+
+
+
+
+
+
+
+
+// Here is a place to put functions
+
+function randomWord() {
+		return words[Math.floor(Math.random() * words.length)];	
+	}
+
+
+
