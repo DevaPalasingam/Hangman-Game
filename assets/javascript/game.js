7bc8873bec500e55d24b5aaf761396210889683e
Deva <deva.palasingam@gmail.com>
2017-10-04 22:10:02 -0500

    working on checkUserLetter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,31 @@
-var words = ["typewriter", "pager", "fax machine", "dial up", "phonebook", "floppy disk", "VHS"];
+var words = ["typewriter", "pager", "fax machine", "dial up", "phonebook", "floppy disk", "vhs"];
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var guessedLetters = [];
+var currentOutput = [];
 var trysLeft = 12;
 var secretWord;
+var gameOver = false;
 
 secretWord = randomWord();
 printBlanks(secretWord);
 
 
+//This function happens when user releases a key
+document.onkeyup = function(event) {
+
+	if (gameOver === true) {
+		return;
+	}
+
+	//this for-loop checks if user pressed a valid letter
+	for (var i = 0; i < letters.length; i++) {
+		if (event.key === letters[i]) {
+			console.log("gottem")
+		}
+	}
+
+}
 
 
 
@@ -26,6 +44,8 @@ function randomWord() {
 	console.log(chosenWord);
 	return chosenWord;
 }
+//.............................................................
+
 
 
 //Input: word | Then prints the proper amount of blanks..........
@@ -36,17 +56,42 @@ function printBlanks(printBlanks_input) {	//
 	
 	for (var i = 0; i < wordLength; i++) {	//This loop will create an array with all the needed blanks and spaces
 		if (printBlanks_input.charAt(i) === " ") {
-			blankArray[i] = '\xa0\xa0';
+			blankArray[i] = '\xa0\xa0\xa0';
 		}
 		else {
-			blankArray[i] = " _";
+			blankArray[i] = " _ ";
 		}
 	}
+
+	currentOutput = blankArray.slice(0);
+	console.log(currentOutput);
 
 	for (var e = 0; e < blankArray.length; e++) {	//This loop turns blankArray into a string
 		printString = printString.concat(blankArray[e]);
 	}
 
 	document.querySelector("#blanks").innerHTML = printString;
+}
+//.............................................................
+
+
+//Input: letter that user pressed | checks if it's one of the correct letters and checks if the letter has already been pressed
+function checkUserLetter(inputLetter) {
+	var newPrint;
+	var alreadyGuessed;
+
+	//this loop checks to see if the letter has already been pressed
+	for (var i = 0; i < guessedLetters.length; i++) {
+		//leaves the function if the letter has already been pressed
+		if (inputLetter === guessedLetters[i]) {
+			return;
+		}
+	}
+
+	//this will check if the letter is correct and will change the current output
+	for (var e = 0; e < secretWord.length; e++) {
+		
+	}
+
 }
 
