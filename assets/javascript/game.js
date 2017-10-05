a8f6503bc7bfdf7e7426e53df17fa2110fc28440
Deva <deva.palasingam@gmail.com>
2017-10-04 20:46:27 -0500

    finished picking word and printing blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,10 @@ var words = ["typewriter", "pager", "fax machine", "dial up", "phonebook", "flop
 
 var guessedLetters = [];
 var trysLeft = 12;
+var secretWord;
 
-var chosenWord = words[Math.floor(Math.random() * words.length)];
-
-var wordLength = chosenWord.length;
-
-
+secretWord = randomWord();
+printBlanks(secretWord);
 
 
 
@@ -16,11 +14,39 @@ var wordLength = chosenWord.length;
 
 
 
-// Here is a place to put functions
 
-function randomWord() {
-		return words[Math.floor(Math.random() * words.length)];	
+
+// Here is a place to put functions...........................
+
+
+//Input: none | Output: random word from list................
+function randomWord() {	
+	var chosenWord;
+	chosenWord = words[Math.floor(Math.random() * words.length)];
+	console.log(chosenWord);
+	return chosenWord;
+}
+
+
+//Input: word | Then prints the proper amount of blanks..........
+function printBlanks(printBlanks_input) {	//
+	var wordLength = printBlanks_input.length;
+	var blankArray = [];
+	var printString = " ";
+	
+	for (var i = 0; i < wordLength; i++) {	//This loop will create an array with all the needed blanks and spaces
+		if (printBlanks_input.charAt(i) === " ") {
+			blankArray[i] = '\xa0\xa0';
+		}
+		else {
+			blankArray[i] = " _";
+		}
 	}
 
+	for (var e = 0; e < blankArray.length; e++) {	//This loop turns blankArray into a string
+		printString = printString.concat(blankArray[e]);
+	}
 
+	document.querySelector("#blanks").innerHTML = printString;
+}
 
